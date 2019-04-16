9f2ba1daa993f7b8a741bd002ce3bf79a00c650b
Hunt Young <yanghong@users.noreply.github.com>
2019-04-16 09:42:23 +0800

    version 20190321

--- src/NonNorm.cpp
+++ src/NonNorm.cpp
@@ -1,16 +1,45 @@
 #include "NonNorm.h"
 #include <fstream>
+#include <cmath>
+#include <list>
 
 MatInfo NONNORM(Mat &D,float rho,Mat &T0,float gamma){
 	Mat U,S,V,outputT,outputPow;
 	MatInfo getMatInfo;
 	MatInfo returnMatInfo;
-	fstream file;
-	file.open("temp.txt");
-    
+	ofstream file("temp.txt",ios::app);
+    int count=0;
     // set patch
-	int patchWidth = 9;
-	int patchHeight = 9;
+	int patchWidth = 25;
+	int patchHeight = 25;
+	int stepsize = 16;
+	int m = D.rows;
+	int n = D.cols;
+	int R = m-patchWidth+1;
+	int C = n-patchWidth+1;
+	list<int> rr;
+	list<int> cc;
+
+	for(int si = 1; si < R; si++) {
+		if(si%stepsize == 1) {
+			rr.push_back(si);
+		}
+	}
+	rr.push_back(rr.back()+1);
+	for(int sj = 1; sj < C; sj++) {
+		if(sj%stepsize == 1){
+			cc.push_back(sj);
+		}
+	}
+	cc.push_back(cc.back()+1);
+
+	int rrLength = rr.size();
+    int ccLength = cc.size();
+	list<int>::iterator rrIter;
+	list<int>::iterator ccIter;
+	rrIter = rr.begin();
+	ccIter = cc.begin();
+
 
 	Mat T0Type;
 	T0.convertTo(T0Type, CV_32FC1);
@@ -19,80 +48,89 @@ MatInfo NONNORM(Mat &D,float rho,Mat &T0,float gamma){
 
 	int patchI = DType.cols/patchWidth;
     int patchJ = DType.rows/patchHeight;
-	file << "patchI: " << patchI << "patchJ: " << patchJ << endl;
-	Mat patch;
-	
-	cout << "patchI: " << patchI << endl;
-	cout << "patchJ: " << patchJ << endl;
-	for (int pi=0;pi<patchI;pi++) {
-		for(int pj=0;pj<patchJ;pj++) {
-			int positionX = pi * patchWidth;
-			int positionY = pj * patchHeight;
-			patch = DType(Range(positionY,positionY + 9),Range(positionX,positionX + 9));
-			file << "positionX: " << positionX << endl;
-			file << "positionY: " << positionY << endl;
-			file << "patch: " << patch << endl;
-		}
-	
-	}
+	Mat result(D.rows,D.cols,CV_32FC1);
+	Mat resultV(patchWidth,D.cols,CV_32FC1);
 
-	// SVD -> D
-	SVD::compute(DType, S, U, V);
 
-	// itera 1:100
-	for (int i = 1; i< 101; i++) {
-		getMatInfo = DCInner(S,rho,T0Type,gamma,U,V);
+	int positionX = 0;
+	int positionY = 0;
 
-		subtract(getMatInfo.T,T0Type,outputT);
-		pow(outputT,2,outputPow);
-		float err;
-		for (int row = 0;row< outputPow.rows;row++) {
-			float *data = outputPow.ptr<float>(row);
-			for(int col=0;col<outputPow.cols;col++) {
-				err = err + data[col];
+	Mat resultHTemp;
+	for (int pi=0;pi<rrLength;pi++) {
+			Mat resultH(stepsize,stepsize,CV_32FC1);
+			if(rrIter != rr.end()) {
+				positionX = *rrIter;
+				++rrIter;
+				ccIter = cc.begin();
 			}
-		}
-		if(err < 1e-6){
-			break;
-		}
-		T0Type = getMatInfo.T;
+		for(int pj=0;pj<ccLength;pj++) {
+			if(ccIter != cc.end()) {
+				positionY = *ccIter;
+				++ccIter;
+			}
+			Mat patch = DType(Range(positionX,positionX + patchWidth),Range(positionY,positionY + patchWidth));
 
+
+			SVD::compute(patch, S, U, V);
+			for (int i101 = 1; i101< 101; i101++) {
+				getMatInfo = DCInner(S,rho,T0Type,gamma,U,V);
+				subtract(getMatInfo.T,T0Type,outputT);
+				pow(outputT,2,outputPow);
+				float err;
+				for (int row = 0;row< outputPow.rows;row++) {
+					float *data = outputPow.ptr<float>(row);
+					for(int col=0;col<outputPow.cols;col++) {
+						err = err + data[col];
+					}   
+				}   
+				if(err < 1e-6){
+					 break;
+				}   
+				getMatInfo.T.copyTo(T0Type);		
+			  }
+			// hconcat must be rows equal
+			Mat resultStepsize = getMatInfo.X(Range(0,stepsize),Range(0,stepsize));
+			if (pj == 0) {
+				resultH = resultStepsize;
+			} else {
+				hconcat(resultH,resultStepsize,resultH);
+			}
+			count++;
+		}
+        
+		if (resultH.cols < D.cols) {
+			int gap = D.cols - resultH.cols;
+			Mat resultFill = getMatInfo.X(Range(0,patchWidth),Range(patchWidth - gap,patchWidth));
+			hconcat(resultH,resultFill,resultH);
+		}
+		// vconcat must be columns equal
+		if (pi == 0) {
+			resultV = resultH;
+		} else {
+			vconcat(resultV,resultH,resultV);
+		}
 	}
 
-	Mat T = getMatInfo.T;
-	returnMatInfo.X = getMatInfo.X;
+    resize(resultV,result,result.size(),INTER_LINEAR);
+	returnMatInfo.X = result;
 	returnMatInfo.T = getMatInfo.T;
 	return returnMatInfo;
 }
 
 MatInfo DCInner(Mat &S, float rho, Mat &J , float epislon, Mat &U, Mat &V) {
+	ofstream file2("temp2.txt",ios::app);
 	MatInfo matInfo;
-	float lambda = 0.5/rho;
+	float lambda = rho;
 	Mat zeroMat = Mat::zeros(J.rows,J.cols,CV_32FC1);
 	Mat outputGrad;
 
-	// Mat S0 = Mat::diag(S);
 	exp(-J/epislon,outputGrad);
 	Mat grad = outputGrad/epislon;
 	Mat t = max(S - lambda*grad,0.0);
 	Mat tDiag = Mat::diag(t);
 	Mat X = U * tDiag * V;
-	matInfo.X = X;
-	matInfo.T = t;
+	X.copyTo(matInfo.X);
+	t.copyTo(matInfo.T);
 	return matInfo;
 }
 
-float NonSum(Mat &mat){
-	ofstream filetemp3;
-	filetemp3.open("temp3.txt");
-	filetemp3 << "mat:" << mat << endl;
-	float s = 0.0f;
-	for(int row=0;row<mat.rows;row++){
-		float *data = mat.ptr<float>(row);
-		for(int col=0;col<mat.cols;col++) {
-			s = s + data[col];
-			filetemp3 << "data[" << col << "]:" << data[col] << endl;
-		}
-	}
-	return s;
-}
